a2a40a0785a70af9e19aed3b10301692c29b23b4
Jan Ruettinger <service+github@janruettinger.com>
2022-04-02 17:31:57 -0700

    simple demo to connect with metamask on harmony testnet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,71 @@
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { InjectedConnector, Provider, chain, defaultChains } from 'wagmi';
+import { providers } from 'ethers';
+import '../styles/index.css';
+
+// const provider = new ApiHarmonyProvider('https://a.api.s0.t.hmny.io/');
+// const provider = new providers.PocketProvider('harmonymainnet');
+// const provider = new providers.Web3Provider(web3.currentProvider);
+const provider = new providers.JsonRpcProvider(
+  'https://api.s0.b.hmny.io',
+  1666700000
+);
+
+// export type Network = {
+//   name: string,
+//   chainId: number,
+//   ensAddress?: string,
+//   _defaultProvider?: (providers: any, options?: any) => any
+// }
+
+// Chains for connectors to support
+const chains = [
+  {
+    id: 1666700000,
+    name: 'Harmony Testnet Shard 0',
+    rpcUrls: ['https://api.s0.b.hmny.io'],
+    nativeCurrency: { name: 'ONE', symbol: 'ONE', decimals: 18 as 18 },
+    blockExplorers: [
+      {
+        name: 'Harmony Block Explorer',
+        url: 'https://explorer.harmony.one',
+        standard: 'EIP3091',
+      },
+    ],
+    testnet: false,
+  },
+];
+
+// blockExplorers: [{…}]
+// id: 1
+// name: "Mainnet"
+// nativeCurrency: {name: 'Ether', symbol: 'ETH', decimals: 18}
+// rpcUrls: ['https://mainnet.infura.io/v3']
+
+console.log('Default chains:', defaultChains);
+
+// Set up connectors
+const connectors = () => {
+  return [
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+  ];
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    // <Provider provider={provider}>
+    <Provider connectors={connectors} provider={provider}>
       <Head>
         <title>NextJS TailwindCSS TypeScript Starter</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
-    </>
-  )
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,92 @@
+import React, { useEffect } from 'react';
+import {
+  useConnect,
+  useAccount,
+  useNetwork,
+  useBalance,
+  useProvider,
+} from 'wagmi';
+
+export const useIsMounted = () => {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  return mounted;
+};
+
 export default function Home() {
+  const [{ data, error }, connect] = useConnect();
+  const [{ data: accountData }, disconnect] = useAccount();
+  const [{ data: balanceData, error: balanceError, loading: balanceLoading }] =
+    useBalance({
+      addressOrName: '0xF39963D2A64Fb7Bb9FC38B34A942678152E5180F',
+    });
+  const [{ data: networkData, error: networkError, loading }, switchNetwork] =
+    useNetwork();
+  const isMounted = useIsMounted();
+
+  if (accountData) {
+    return (
+      <div>
+        <img src={accountData.ens?.avatar} alt='ENS Avatar' />
+        <div>
+          {accountData.ens?.name
+            ? `${accountData.ens?.name} (${accountData.address})`
+            : accountData.address}
+        </div>
+        <div>Connected to {accountData.connector.name}</div>
+        <button onClick={disconnect}>Disconnect</button>
+        <div>
+          {networkData.chain?.name ?? networkData.chain?.id}{' '}
+          {networkData.chain?.unsupported && '(unsupported)'}
+        </div>
+        {balanceLoading && <div>Fetching balance…</div>}
+        {balanceError && <div>Error fetching balance</div>}
+        <div>
+          {balanceData?.formatted}{' '}
+          {networkData.chains[0]?.nativeCurrency.symbol}
+        </div>
+
+        {switchNetwork &&
+          networkData.chains.map((x) =>
+            x.id === networkData.chain?.id ? null : (
+              <button key={x.id} onClick={() => switchNetwork(x.id)}>
+                Switch to {x.name}
+              </button>
+            )
+          )}
+
+        {error && <div>{error?.message}</div>}
+      </div>
+    );
+  }
+
   return (
-    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+    <div className='container flex items-center p-4 mx-auto min-h-screen justify-center'>
       <main>
-        <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
-        </h1>
+        <div>
+          {data.connectors.map((connector) => {
+            console.log('connector: ', connector);
+            console.log('connector ready: ', connector.ready);
+            return (
+              <button
+                className='bg-blue-700 text-white p-4'
+                disabled={isMounted ? !connector.ready : false}
+                key={connector.id}
+                onClick={() => connect(connector)}
+              >
+                {isMounted
+                  ? connector.name
+                  : connector.id === 'injected'
+                  ? connector.id
+                  : connector.name}
+                {!connector.ready && '(unsupported)'}
+              </button>
+            );
+          })}
+
+          {error && <div>{error?.message ?? 'Failed to connect'}</div>}
+        </div>
       </main>
     </div>
-  )
+  );
 }
